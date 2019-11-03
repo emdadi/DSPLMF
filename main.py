da861f64935394d6f046b98e8507187605cd0ca0
emdadi <35139148+emdadi@users.noreply.github.com>
2019-11-03 16:31:37 +0330

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ import os
 from sklearn.model_selection import  RepeatedKFold
 from function import load_matrices, apply_threshold, compute_evaluation_criteria
 from lmf import PSLMF
-from function import apply_threshold_median,score_to_exact_rank,pearson_correlation_acros_cell_lines,pearson_correlation_acros_drugs
+from function import apply_threshold_median,score_to_exact_rank
 
-from function import compute_evaluation_criteria_across_wholeMatrix,spearman_correlation_acros_cell_lines,spearman_correlation_acros_drugs
+from function import compute_evaluation_criteria_across_wholeMatrix
 import numpy as np
 
 
@@ -33,15 +33,10 @@ F1, ACC , ACC_new, F1_score_new= 0.0, 0.0, 0.0, 0.0
 REC_new=0.0
 Specificity=0.0
 MCC_new=0.0
-AUC_new=0.0
-AUC_new_new=0.0
+
+AUC=0.0
 precision_new=0.0
 result=0.0
-
-result_cor_cell_lines=0.0
-result_cor_drugs=0.0
-result_speraman_cor_cell_lines=0.0
-result_speraman_cor_drugs=0.0
 
 
 
@@ -72,7 +67,7 @@ for train_index, test_index, in kf.split(proteins_sim, observation_mat):
     prediction = apply_threshold(scores, 0.6)
    
    
-    precision_new_o,fold_f1, fold_acc,fold_ACC_new ,fold_F1_score_new,REC_new_output,Specificity_new,MCC_new_output,AUC_new_o,AUC_new_new_o,fold_loc_f1 = compute_evaluation_criteria_across_wholeMatrix(true_result, prediction,scores)
+    precision_new_o,fold_f1, fold_acc,fold_ACC_new ,fold_F1_score_new,REC_new_output,Specificity_new,MCC_new_output,AUC_new_new_o,fold_loc_f1 = compute_evaluation_criteria_across_wholeMatrix(true_result, prediction,scores)
     F1+=fold_f1
     ACC+=fold_acc
     ACC_new+=fold_ACC_new
@@ -82,7 +77,7 @@ for train_index, test_index, in kf.split(proteins_sim, observation_mat):
     MCC_new=MCC_new_output+MCC_new
     AUC_new=AUC_new_o+AUC_new
     precision_new=precision_new_o+precision_new
-    AUC_new_new=AUC_new_new_o+AUC_new_new
+    AUC=AUC_new_new_o+AUC_new_new
    
     observation_mat_IC50_mat = np.array(observation_mat_IC50)
    
@@ -92,12 +87,7 @@ for train_index, test_index, in kf.split(proteins_sim, observation_mat):
     pred=scores 
    
     
-    result_cor_cell_lines+=pearson_correlation_acros_cell_lines(RR,pred)
-    result_cor_drugs+=pearson_correlation_acros_drugs(RR,pred) 
-    result_speraman_cor_cell_lines+= spearman_correlation_acros_cell_lines (RR,pred)
-    result_speraman_cor_drugs+=spearman_correlation_acros_drugs(RR,pred)
-    
-    
+   
     
 F1=round(F1/(10*num_repeats),2)
 ACC=round(ACC/(10*num_repeats),2)
@@ -106,15 +96,10 @@ F1_score_new=round(F1_score_new/(10*num_repeats),2)
 REC_new=round(REC_new/(10*num_repeats),2)
 MCC_new=round(MCC_new/(10*num_repeats),2)
 Specificity=round(Specificity/(10*num_repeats),2)
-AUC_new=round(AUC_new/(10*num_repeats),2)
+
 precision_new=round(precision_new/(10*num_repeats),2)
-AUC_new_new=round(AUC_new_new/(10*num_repeats),2)
+AUC=round(AUC_new_new/(10*num_repeats),2)
 
-
-result_cor_cell_lines=round(result_cor_cell_lines/(10*num_repeats),2)
-result_cor_drugs=round(result_cor_drugs/(10*num_repeats),2)
-result_speraman_cor_cell_lines=round(result_speraman_cor_cell_lines/(10*num_repeats),2)
-result_speraman_cor_drugs=round(result_speraman_cor_drugs/(10*num_repeats),2)
 
 
 print('F1',F1, 'ACC',ACC)
@@ -123,14 +108,10 @@ print('F1_score_new', F1_score_new)
 print('REC_new', REC_new)
 print('Specificity', Specificity)
 print('MCC_new', MCC_new)
-print('AUC_new', AUC_new)
+print('AUC', AUC)
 print('precision_new', precision_new)
-print('AUC_new_new', AUC_new_new)
 
 
-print('result_cor_cell_lines',result_cor_cell_lines)    
-print('result_cor_drugs',result_cor_drugs)
-print('result_speraman_cor_cell_lines',result_speraman_cor_cell_lines)
-print('result_speraman_cor_drugs',result_speraman_cor_drugs)
+
 
 
